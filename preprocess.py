e3a65e643b91f5d265a9d67e1b6eac00f70a95d1
Dario Manser <d.manser@stud.unibas.ch>
2026-09-07 11:45:55 +0200

    .

--- preprocess.py
+++ preprocess.py
@@ -9,10 +9,13 @@ def sub(m):
     lines = m.group(0).strip().split('\n')
     stmts = []
     for line in lines:
-        stmt = line[2:]
-        for prefix, color in {'!': 'red', '*': 'green!75!black'}.items():
-            if stmt.startswith(f'{prefix} '):
-                stmt = f'{{\\color{{{color}}}{stmt.removeprefix(f'{prefix} ')}}}'
+        stmt = line[1:]
+        if stmt.startswith("'"):
+            stmt = f'\\textit{{{stmt.removeprefix("'")}}}'
+        else:
+            for prefix, color in {'!': 'red', '*': 'green!75!black'}.items():
+                if stmt.startswith(f'{prefix}'):
+                    stmt = f'{{\\color{{{color}}}{stmt.removeprefix(f'{prefix}')}}}'
         stmts.append(stmt)
     return f'\\comment{{{'\\\\'.join(stmts)}}}\n'
 
@@ -24,6 +27,6 @@ for f in Path('content').glob('*.tex'):
     if True or not f_.is_file() or f.stat().st_mtime > f_.stat().st_mtime:
         text = f.read_text(encoding='utf-8')
 
-        text = re.sub(r'(?:^% .+$\n?)+', sub, text, flags=re.MULTILINE)
+        text = re.sub(r'(?:^%[^ ].+$\n?)+', sub, text, flags=re.MULTILINE)
 
         f_.write_text(text)
